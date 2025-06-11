d17d4b83fe27c87dcdc1cd8382d68354b343b06a
Alan Montoya <gazalem@hotmail.com>
2025-06-11 17:14:03 -0600

    Optimized image for loading

--- character-card/js/character.js
+++ character-card/js/character.js
@@ -3,7 +3,7 @@ const character = {
   class: "Swamp Beat Diplomat",
   level: 5,
   health: 100,
-  image: "https://andejuli.github.io/img/snortleblat.png",
+  image: "./img/snortleblat.avif",
   attacked() {
     if (this.health >= 20) {
       this.level -= 1;
@@ -17,3 +17,31 @@ const character = {
     this.health += 20;
   },
 };
+
+
+const img = document.querySelector(".image");
+img.src = character.image;
+const name = document.querySelector(".name");
+name.textContent = character.name;
+const characterClass = document.querySelector("#class");
+characterClass.textContent = character.class;
+const characterLevel = document.querySelector("#level");
+characterLevel.textContent = character.level;
+const characterHealth = document.querySelector("#health");
+characterHealth.textContent = character.health;
+
+// buttons functionality
+const attacked = document.querySelector("#attacked");
+const levelUp = document.querySelector("#levelup");
+
+attacked.addEventListener("click", () => {
+  character.attacked();
+  characterLevel.textContent = character.level;
+  characterHealth.textContent = character.health;
+});
+
+levelUp.addEventListener("click", () => {
+  character.levelUp();
+  characterLevel.textContent = character.level;
+  characterHealth.textContent = character.health;
+})
