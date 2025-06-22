230a5a9db7523a925c336c0835c826d42f69f542
Alan Montoya <gazalem@hotmail.com>
2025-06-22 12:47:58 -0600

    Removed JS, fix for the button and the input element to be cosistent on all browsers and sizes

--- recipes/js/recipes.js
+++
@@ -1,7 +0,0 @@
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-searchButton.addEventListener('load', () => {
-  console.log(searchInput.style.height);
-  searchButton.style.height = searchInput.style.height;
-});
-console.log(searchButton.style.height);
